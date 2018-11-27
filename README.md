6a54bbde7fffda4ff889b858b4cfac4ee56bfa84
Aaron Paterson <juniha.aaron@gmail.com>
2018-11-27 18:56:08 -0500

    updating olypimic files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # python_DataViz_B
+
+
+Brief: 
+
+
+Task: 
+<!-- competing men and women form 1924 -2012 gold medals -->
+1 competing men v. women years medals 
+
+<!-- are we winning more or less -->
+2 competing canada v. world 
+
+<!-- competing men and women in canada  -->
+3 competing men v. women in canada 
--- canadaWorld.py
+++ canadaWorld.py
@@ -13,16 +13,16 @@ with open('data/OlympicsWinter.csv') as csvfile:
     for row in reader:
         if line_count is 0:
             categories.append(row)
-            line_count += 1
+            line_count += 1;
 
-        else row(4) == "CAN":
+        elif row[4] == "CAN":
             canada.append([int(row[0]), row[5], row[6], row[7]])
         else: 
             world.append([int(row[0]), row[5], row[6], row[7]])
             line_count =+ 1
 
 print('total medals for canada:', len(canada))
-print('rest of the world:' len(world))
+print('rest of the world:', len(world))
 
 print(canada[0])
 
@@ -46,9 +46,11 @@ for medal in canada:
     elif medal[0] == 2014 and medal[3] == "Gold":
         gold_2014.append(medal)
 
-print('canada won'), len(gold_1942),  'gold medals in 1924')
-print(' canada won'  len(gold_2014), 'gold medals in 2014')
+print('canada won', len(gold_1924),  'gold medals in 1924')
+print(' canada won',  len(gold_2014), 'gold medals in 2014')
 
+
+#  Array bucket for men and women parse
 men_1924 = []
 women_1924 = []
 men_1948 = []
@@ -57,25 +59,27 @@ men_1972 = []
 women_1972 = []
 men_2002 = []
 women_2002 = []
-men_2014 = [][]
+men_2014 = []
 women_2014 = []
 
-for men in canada
-    if men[0] == 1924 and women[1] == "Gold"
-        men_1924.append(women)
-    elif men[0] == 1948 and women[1] == "Gold"
-        men_1948.append(women)
-    elif men[0] == 1972 and women[1] == "Gold"
-       men_1972.append(women)
-    elif men[0] == 2002 and women[1] == "Gold"
-        men_2002.append(women)
-    elif men[0] == 2014 and women[1] == "Gold"
-        men_2014.append(women)
+# Loop that is fetching the excel sheet from 
+# first if state we'll check the year[0] which is the years in csv, the if means the years[1] is referencing the gender then it's appending to another array.
+# [0] = years, [1] gender, [2] = event, [3] = medals
+for years in canada:
+    if years[0] == 1924 and years[3] == "Gold":
+    # here must be 1924 and gold to execute
+        if years[1] == "Men": 
+            men_1924.append(years)
+        elif years[1] == "Women": 
+            women_1924.appending(years)
+    # end if 1924 and gold
 
-print('men'), len(gold_1924), 'gold medals in 1924')
-print('men'), len(gold_1948), 'gold medals in 1948')
-print('men'), len(gold_1972), 'gold medals in 1972')
-print('men'), len(gold_2012), 'gold medals in 2012')
-print('men'), len(gold_2014), 'gold medals in 2014')
+print('men', len(gold_1924), 'gold medals in 1924')
+print('men', len(gold_1948), 'gold medals in 1948')
+print('men', len(gold_1972), 'gold medals in 1972')
+print('men', len(gold_2002), 'gold medals in 2012')
+print('men', len(gold_2014), 'gold medals in 2014')
 
+
+#  
 
